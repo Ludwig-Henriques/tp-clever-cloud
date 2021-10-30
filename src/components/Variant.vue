a5d3dfb3231ecf3f9a223421edfb718604b84389
Ludwig <ludwig.henriques@gmail.com>
2021-10-30 13:31:23 +0200

    [CHANGE] component variants with static data and add component variant

---
+++ src/components/Variant.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="variant">
+    <img :src="logo" >
+    <span>{{name}}</span>
+    <button>Select</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Variant',
+  props: {
+    name: String,
+    logo: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.variant {
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  padding: 10px;
+}
+
+.variant img {
+  width: 50px;
+  margin-right: 10px;
+}
+
+.variant button {
+  margin-left: auto;
+}
+
+</style>
