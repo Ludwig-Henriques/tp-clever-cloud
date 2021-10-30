ab9313d45ed1096bede88dd8235047c9ea64cd03
Ludwig <ludwig.henriques@gmail.com>
2021-10-30 12:19:13 +0200

    [ADD] add responsive layout with grid

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div class="total">Total</div>
+      <div class="variants">Variants
+      </div>
+      <div class="flavors">
+        Flavors
+      </div>
+      <div class="cart">
+        Cart
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
 
 <style>
+body {
+  margin:0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  grid-template-rows: 80px 1fr;
+  grid-gap: 20px;
+  box-sizing: border-box;
+  padding: 20px;
+}
+
+.total{
+  background-color: orange;
+  grid-column: 1 / -1;
+}
+
+.variants {
+  background-color: blue;
+  overflow-y: scroll;
+}
+.flavors {
+  background-color: red;
+  overflow-y: scroll;
+}
+.cart {
+  background-color: green;
+  overflow-y: scroll;
 }
 </style>
