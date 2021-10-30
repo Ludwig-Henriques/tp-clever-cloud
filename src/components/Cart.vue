b02cb10c36a9741c8428b4802f101d236c26534c
Ludwig <ludwig.henriques@gmail.com>
2021-10-30 13:59:29 +0200

    [CHANGE] component cart with static data and add component item

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="cart">
-    Cart
+    <Item v-for="item in items" :key="item.nameVariant+item.nameFlavor" :nameVariant="item.nameVariant" :nameFlavor="item.nameFlavor" :price="item.price" />
   </div>
 </template>
 
 <script>
+import Item from './Item.vue'
+
 export default {
   name: 'Cart',
+  components: {
+    Item
+  },
+  data() {
+    return{
+      items: [
+        {
+          nameVariant: "Docker",
+          nameFlavor: "nano",
+          price: "16"
+        },
+        {
+          nameVariant: "PHP",
+          nameFlavor: "nano",
+          price: "10"
+        }
+      ]
+    }
+  }
 }
 </script>
 
---
+++ src/components/Item.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="item">
+    <span>{{nameVariant}}</span>
+    <span>{{nameFlavor}}</span>
+    <span>{{price}}</span>
+    <button>Remove</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Item',
+  props: {
+    nameVariant: String,
+    nameFlavor: String,
+    price: String,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.item {
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  padding: 10px;
+  min-height: 50px;
+}
+
+.item span {
+  margin-left: 10px;
+}
+
+.item button {
+  margin-left: auto;
+}
+
+</style>
