5fb9865aff49e453618a94fdc40e19caed8c2729
Ludwig <ludwig.henriques@gmail.com>
2021-10-31 09:56:06 +0100

    [ADD] add api request to get variants data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
@@ -9,3 +10,12 @@ new Vue({
   store,
   render: h => h(App)
 })
+
+
+axios.get('https://api.clever-cloud.com/v2/products/instances')
+  .then((res) => {
+    store.dispatch('updateVariants', res.data)
+  })
+  .catch((error) => {
+    console.error(error)
+  })
